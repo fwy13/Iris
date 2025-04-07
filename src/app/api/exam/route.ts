2a884865198be2e67820ebe8ae574b517d63f8e2
fwy13 <quyhuynh1309@gmail.com>
2025-04-07 17:36:03 +0700

    chore: fix db

--- src/app/api/exam/route.ts
+++ src/app/api/exam/route.ts
@@ -1,9 +1,7 @@
-import { addData } from "@/db/addData";
 import { NextResponse } from "next/server";
 
 export async function GET() {
 
-    await addData();
     return NextResponse.json({
         error: false,
         data: [],
